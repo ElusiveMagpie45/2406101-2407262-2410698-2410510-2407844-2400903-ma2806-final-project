9c8a3ba6d5a8d559d38aad41eadf043cb5ee976e
oli-c2004 <161333805+oli-c2004@users.noreply.github.com>
2024-03-18 18:46:56 +0000

    Add files via upload

---
+++ 2406101-2407262-2410698-2410510-2407844-2400903-ma2806-final-project-main/README.md
@@ -0,0 +1,39 @@
+Traversing Shadows
+
+BY A-maze-ing Crew
+MEMBERS: AJ Heffernan (2406101), Ollie Chlopek (2407262), Huseyin Hassan (2410698), Cherry CHE (2410510), Amelia Baxter (2407844), Bani Kaur (2400903)
+
+OVERVIEW OF GAME
+
+Our game is about a person who is lost in a completely dark, limbo-like, maze-forest environment, with only a small torch to guide them through the darkness that lights up their immediate area. They are a lone plane crash survivor who has to return to civilization by making it through the dark forest and thus the levels of the game. They will have to avoid enemy wild animals such as wolves and bears, with a mechanic of batteries (extending the torch's range) and guns being able to be picked up to fend these off. The most important mechanic is the small torchlight that surrounds the player, as it is the only means to see the environment and hopefully escape. We  have gone for a dark aesthetic, though done in pixel art, so while it may appear as cutesy pixel art at first glance, it's really a dark horror aesthetic behind this facade.
+Our game is played simply controlled with the WASD control scheme so far, with E or similar hopefully implemented for use on items in the final game.
+
+INDIVIDUAL CONTRIBUTIONS
+
+AJ -  I have set up and managed the repository, written the README, along with gathering the prior code of tile map and player movement from workshops to use as a foundation for our own, and aided in merging code to the main code. I have set up code, using inspiration from online particularly for working around Chrome's playback policies, that creates a loop of music that I have sampled and mixed to fit the creepy aesthetic. I have also helped with graphics, retooling the obstacles to be trees, fixing the guns and batteries to blend with the grass tiles and scaled them down to fit the rest of the graphics, and created the log tile to differentiate the environment 
+
+Ollie - I have reorganised and fixed the tile map code, aided in consolidating code, in addition to suggesting the thematic and aesthetic inspiration for our project.
+
+Huseyin - I have implemented the code for the torch mechanic, lighting the area surrounding the player, using inspiration from a tutorial I found online for guidance. I have also made it so that the powerups are not visible without being in the spotlight, remaining hidden in the darkness if not in the lit area around the player. I made it so that when you pick up the battery the whole map is visible for 5 seconds before reverting back to the torch.
+
+Cherry - I have coded the items that can be picked up, the guns and batteries, using inspiration I found online for the code, as well as inputting their positions to be randomized across the map, though not on inaccessible tiles, and designing their appearance.
+
+Amelia - I have found our aesthetic inspiration through a Pinterest board of images and have drawn and helped implement the sprites and texture design for the player and environment, using a pixel art aesthetic throughout the designs as an art style that serves as a thematic facade. I am also working on the code for a start menu, that shows instructions and gives a basic backstory of the game. this is currently commented out as when writing the code, the index for the project wouldn't load and i therefore couldn't see the effects of the new code, making it difficult to immplement.
+
+Bani - I helped with the narrative development and theme of the game, and I have implemented the code for the enemies, possibly making use of code from prior lectures, i am struggling to display the enemy right now, I am going to figure this out soon and add more enemies such as wolves and bear. I also need to add a gun mechanic where a bullet shot will cause the enemy to dissappear. 
+
+THEMES
+
+We are going for a horror, dark theme, focusing on a person lost in a dark forest with only a small light for guidance. The structure will be that of a maze, with a claustrophobic, scary environment, with all but the player's immediate area enveloped in darkness.
+
+GAME DESIGN
+
+The game explores themes of isolation and survival, with the player being completely alone in an almost completely dark environment save for the small spotlight from their torch. the traversal of the crampt obstacles combined with this mostly dark environment aids that feeling of isolation and the fear desired for a horror-based game. We aim to have the items fully useable, both the gun as a weapon against wild creature enemies and the batteries as a temporary increase to the torch, as well as fully implement the enemies that as of yet are not present at all. The game features the key gameplay loop of "Exploration -> Challenge -> Reward", as the player must explore the environment to find a challenge, whether that be an enemy or a new item, with a reward of survival or utilising said item.
+
+NARRATIVE
+
+The game starts with the player waking up in a creepy, dark forest. They have just survived a plane crash and are miles from proper civilisation. They need to survive this dark forest and make it back to civilsations, fighting off the wild creatures that may attack, while resourcefully making good use out of the batteries and guns that have fallen as debris from the crash. In the final project, it may be that we have an intro with text detailing the situation, and perhaps an outro that details their return to civilisation. We may also add detail to the environment such as debris to make it more immersive to the plane crash setting.
+
+AESTHETIC
+
+Our aesthetic is a dark, but pixel-art, one, as though it may appear cutesy pixel art, it is actually a dark horror-esque aesthetic given the dark ennvironment and claustrophobic obstacles layout. Enemies will also have a creepy design when implemented, and we are considering more horror elements i.e. jumpscares. Our main source of inspiration is the game Don't Starve (2013) as suggested by Ollie, as its dark and creepy aesthetic is very much what we are attempting to emulate. Furthermore, images found on Pinterest have inspired our pixel-art style and the forest environment we have portrayed. Such images can be found in the "inspoImages" directory on the repository.
---
+++ 2406101-2407262-2410698-2410510-2407844-2400903-ma2806-final-project-main/sketch.js
@@ -0,0 +1,609 @@
+let tilemap = [];
+let numDown = 20;
+let numAcross = 20;
+let tileSize = 50;
+let textures = [];
+let currentLevel;
+let enemies = [];
+let numEnemies = 5; 
+let music;
+let sfx;
+let torchClick = false;
+
+// Player Variable Initialisation
+    let player;
+    let playerSprite;
+    let playerSpeed = 5;
+    let playerSize = tileSize;
+    let gunSprite;
+    let batterySprite;
+    let enemySprite; 
+    let enemySpeed; 
+
+function preload() {
+    //loads tile textures
+    textures[0] = loadImage("plain.png")
+    textures[1] = loadImage("newTree.png")
+    textures[2] = loadImage("log.png")
+    textures[3] = loadImage("treeLow.png")
+    textures[4] = loadImage("treeTop.png")
+    textures[5] = loadImage("treeTwo.png")
+
+
+    //loads player image
+    playerSprite = loadImage("survivor.png")
+    gunSprite = loadImage("newBattery.png")
+    batterySprite = loadImage("gun.png") 
+    enemySprite = loadImage("enemySprite.gif.png")
+
+    //loads ambient music
+    music = loadSound("ambientMusic.wav")
+    sfx = loadSound("flashlight.mp3")
+
+
+}
+function setup() {
+    createCanvas(1000, 1000);
+
+    loadLevel(beginningObject); //sets global tileMap variables
+    createTileMap(); //creates a variable on the loaded level
+
+    //plays ambient music
+    music.loop();
+    
+    let tileID = 0; //this is the ID for the first tile
+
+    //creates the other tiles
+    for (let across = 0; across < numAcross; across++) {
+        tilemap[across] = [];
+        for (let down = 0; down < numDown; down++) {
+            let x = across * tileSize; //multiplies for pixel position on X
+            let y = down * tileSize; //multiplies for pixel position on Y
+
+            //Texture for Tile
+            let textureNum = graphicMap[down][across];
+
+            let item=null; //init item
+            if(random() < 0.05 && textureNum == 0){
+                item = random() < 0.5 ? gunSprite : batterySprite;
+            }
+
+            tilemap[across][down] = new Tile(textures[graphicMap[down][across]], x, y, tileSize, tileID,item);
+
+            tileID++;
+
+
+            
+        }
+    }
+  
+
+    // Creating player
+    player = new Player(playerSprite, 0, 4, tileSize, playerSpeed, tileSize, batterySprite,gunSprite)
+
+    // Creating enemies
+    for (let i = 0; i < numEnemies; i++) {
+        let enemyX = random(width);
+        let enemyY = random(height);
+        enemies.push(new Enemy(enemyX, enemyY, tileSize, tileSize, enemySprite, enemySpeed));
+    }
+}
+
+
+    //console.log(tilemap[4][6].tileID)
+    //console.log(tilemap[4][6].x / tileSize);
+
+
+class Enemy {
+    constructor(sprite, startAcross, startDown, size, speed, tileSize) {
+      
+        this.sprite = sprite;
+
+     
+        this.across = startAcross;
+        this.down = startDown;
+
+     
+        this.xPos = this.across * tileSize;
+        this.yPos = this.down * tileSize;
+
+      
+        this.size = size;
+        this.speed = speed;
+
+        this.tileSize = tileSize;
+
+     
+        this.dirX = 0;
+        this.dirY = 0;
+
+ 
+        this.isMoving = false;
+
+        this.tx = this.xPos;
+        this.ty = this.yPos;
+    }
+
+    // Set direction of the enemy
+    setDirection() {
+        if (!this.isMoving) {
+            // Randomize movement direction
+            let direction = floor(random(4));
+            switch (direction) {
+                case 0: // UP
+                    this.dirX = 0;
+                    this.dirY = -1;
+                    break;
+                case 1: // DOWN
+                    this.dirX = 0;
+                    this.dirY = 1;
+                    break;
+                case 2: // LEFT
+                    this.dirX = -1;
+                    this.dirY = 0;
+                    break;
+                case 3: // RIGHT
+                    this.dirX = 1;
+                    this.dirY = 0;
+                    break;
+            }
+            // Check the target tile
+            this.checkTargetTile();
+        }
+    }
+
+    // Check target tile for the enemy
+    checkTargetTile() {
+        // Obtain the tile that the enemy is standing on
+        this.across = Math.floor(this.xPos / this.tileSize);
+        this.down = Math.floor(this.yPos / this.tileSize);
+
+        // Calculate the coordinates of the desired tile
+        let nextTileHorizontal = this.across + this.dirX;
+        let nextTileVertical = this.down + this.dirY;
+
+        // Check if the next tile is within the map and if it's a walkable tile
+        if (
+            nextTileHorizontal >= 0 &&
+            nextTileHorizontal < numAcross &&
+            nextTileVertical >= 0 &&
+            nextTileVertical < numDown &&
+            tileRules[nextTileVertical][nextTileHorizontal] != 1
+        ) {
+            // Update the target tile coordinates
+            this.tx = nextTileHorizontal * this.tileSize;
+            this.ty = nextTileVertical * this.tileSize;
+            // Set the enemy to moving
+            this.isMoving = true;
+        }
+    }
+
+    // Move the enemy
+    move() {
+        if (this.isMoving) {
+            this.xPos += this.speed * this.dirX;
+            this.yPos += this.speed * this.dirY;
+
+            if (this.xPos === this.tx && this.yPos === this.ty) {
+                this.isMoving = false;
+                this.dirX = 0;
+                this.dirY = 0;
+            }
+        }
+    }
+
+    // Display the enemy
+    display() {
+        imageMode(CORNER);
+        image(this.sprite, this.xPos, this.yPos, this.size, this.size);
+    }
+}
+
+function createTileMap() {
+let tileID = 0; //sets tileID for the first tile
+
+//Creates rest of tiles
+for(let across = 0; across < numAcross; across++) {
+    tilemap[across] = [];
+    for(let down = 0; down < numDown; down++) {
+        let textureNum = graphicMap [down][across];
+
+        tilemap[across][down] = new Tile (textures[textureNum], across, down, tileSize, tileID); // Creates new tile
+
+        tileID++;
+    }
+}
+
+    addTransitionsToTiles();
+
+}
+
+function addTransitionsToTiles() {
+    if (currentLevel === beginningObject) {
+        currentLevel.tileRules [0][8] = transitionBeginningToMiddle;
+    }
+   
+    if (currentLevel === beginningObject) {
+        currentLevel.tileRules [9][2] = transitionMiddleToBeginning;
+        currentLevel.tileRules [0][2] = transitionMiddleToFinal;
+    }
+
+    if (currentLevel === beginningObject) {
+        currentLevel.tileRules [9][2] = transitionFinalToMiddle;
+    }
+}
+
+
+
+
+function loadLevel(levelToLoad) {
+    currentLevel = levelToLoad; //set curent level as the level that is loading
+    tilemap = []; //a clean tile map
+
+    //level variables
+    numAcross = levelToLoad.numAcross;
+    numDown = levelToLoad.numDown;
+    graphicMap = levelToLoad.graphicMap;
+    tileRules = levelToLoad.tileRules;
+}
+
+function draw() {
+    background(0);
+    
+    player.display();
+    player.move();
+    player.updateBatteryEffect(); //update battery
+
+  // Calculate the visibility area around the player
+  let startX = max(0, player.across - floor(spotlightRadius / tileSize));
+  let endX = min(numAcross - 1, player.across + floor(spotlightRadius / tileSize));
+  let startY = max(0, player.down - floor(spotlightRadius / tileSize));
+  let endY = min(numDown - 1, player.down + floor(spotlightRadius / tileSize));
+  
+    for (let across = 0; across < numAcross; across++) {
+        for (let down = 0; down < numDown; down++) {
+            let currentTile = tilemap[across][down];
+            if (currentTile.item && dist(player.xPos, player.yPos, currentTile.x, currentTile.y) < tileSize) {
+                player.pickUpItem(currentTile.item);
+                currentTile.item = null; // items are removed from the map after being picked up
+            }
+        }
+    }
+    //A loop of tiles for every time that draw function is used
+    for (let across = 0; across < numAcross; across++) {
+        for (let down = 0; down < numDown; down++) {
+            
+            //tilemap[across][down].debug(); //runs a debug on every tile
+            tilemap[across][down].display(); //runs a display for every tile
+
+        }
+    }
+    player.display();
+    player.move();
+}
+
+function keyPressed() {
+    player.setDirection();
+}
+
+class Player {
+    constructor(sprite, startAcross, startDown, size, speed, tileSize, gunSprite,batterySprite) {
+        //Sprite to key object
+        this.sprite = sprite;
+
+        //Starting tile info storage
+        this.across = startAcross;
+        this.down = startDown;
+        //items props
+        this.hasGun = false;
+        this.hasBattery = false;
+        this.gunSprite = gunSprite;
+        this.batterySprite = batterySprite;
+        this.gunUsed = false;
+        this.batteryUsed = false;
+        this.batteryTime = 0;
+
+        //tile co-ords into pixel co-ords
+        this.xPos = this.across * tileSize;
+        this.yPos = this.down * tileSize;
+
+        //size + speed
+        this.size = size;
+        this.speed = speed;
+
+        //rules regarding movement/collision
+        this.tileSize = tileSize;
+
+        //properties of player movement + direction of movement
+        this.dirX = 0;
+        this.dirY = 0;
+
+        //decides when a player is moving to another tile currently
+        this.isMoving = false;
+
+        //x y of the tile in which the player is moving towards
+        this.tx = this.xPos;
+        this.ty = this.yPos;
+
+        this.batteryEffectTimer = 0;
+        this.originalSpotlightRadius = spotlightRadius;
+        this.spotlightExpanding = false;
+
+    } 
+pickUpItem(item) {
+    if (item === this.batterySprite) {
+        this.hasBattery = true;
+        //play torch sound
+        torchClick = true;
+        // Apply battery effect
+        this.applyBatteryEffect();
+        
+    } else if (item === this.gunSprite) {
+        this.hasGun = true;
+        
+    }
+}
+
+
+    applyBatteryEffect() {
+        // Set the spotlight radius to see whole map
+        spotlightRadius = 10000;
+        this.spotlightExpanding = true;
+        this.batteryEffectTimer = 5 * 60; // 5 seconds
+    }
+
+    updateBatteryEffect() {
+        if (this.spotlightExpanding && this.batteryEffectTimer > 0) {
+            this.batteryEffectTimer--;
+            if (this.batteryEffectTimer === 0) {
+                // Reset spotlight radius and effect
+                spotlightRadius = this.originalSpotlightRadius;
+                this.spotlightExpanding = false;
+            }
+        }
+    }
+    // use gun
+    useGun() {
+        if (this.hasGun && !this.gunUsed) {
+            // kill 
+            this.gunUsed = true;
+        }
+    }
+
+    // use battery makes lightly
+    useBattery() {
+        if (this.hasBattery && !this.batteryUsed) {
+            // 10s
+            this.batteryUsed = true
+            this.batteryTime = 10 * 60; //  
+        }
+    }
+    updateBattery() {
+        if (this.batteryUsed && this.batteryTime > 0) {
+            this.batteryTime--;
+            if (this.batteryTime === 0) {
+                this.batteryUsed = false;
+            }
+        }
+    }
+    //WASD and the directions
+    setDirection() {
+        if (!this.isMoving) {
+            
+            //UP
+            if (key === "w") {
+                this.dirX = 0;
+                this.dirY = -1; //direction is up!
+            }
+
+            //DOWN
+            if (key === "s") {
+                this.dirX = 0;
+                this.dirY = 1; //direction is down!
+            }
+
+            //LEFT
+            if (key === "a") {
+                this.dirX = -1; //direction is left!
+                this.dirY = 0; 
+            }
+
+            //RIGHT
+            if (key === "d") {
+                this.dirX = 1; //direction is right!
+                this.dirY = 0;
+            }
+            
+            //the direction of the player is set!
+            this.checkTargetTile();
+        }
+    }
+ //decides what tile the player wants to move towards and if there is a collision there.
+    checkTargetTile() {
+       //obtains the tile that the player is standing on
+        this.across = Math.floor(this.xPos / this.tileSize);
+        this.down = Math.floor(this.yPos / this.tileSize);
+        
+        //finds out the co-ords of the desired tile
+        let nextTileHorizontal = this.across + this.dirX;
+        let nextTileVertical = this.down + this.dirY;
+
+// calculates the availability of the tile (in terms of whether it's within bounds)
+    if (
+        nextTileHorizontal >= 0 && //top
+        nextTileHorizontal < numAcross && //bottom
+        nextTileVertical >= 0 && //left edge
+        nextTileVertical < numDown //right edge
+    ) {
+        //if within map then moveable (using ruleMap)
+        if (typeof tileRules[nextTileVertical][nextTileHorizontal] === "object") {
+
+        //if yes:
+        this.dirX = 0;
+        this.dirY = 0;
+
+        currentLevel = nextLevel.linkedLevel;
+        player.xPos = nextLevel.startingXPos;
+        player.yPos = nextLevel.startingYPos;
+
+        loadLevel(currentLevel);
+        createTileMap();
+
+
+
+
+        
+        //walkable tiles get the x and y co-ords calculated
+        this.tx = nextTileHorizontal * this.tileSize;
+        this.ty = nextTileVertical *this.tileSize;
+        //set to moving character
+        this.isMoving = true;
+       
+       
+        return;
+        }
+    }
+}
+
+move() {
+    if (this.isMoving) {
+        this.xPos += this.speed * this.dirX;
+        this.yPos += this.speed * this.dirY;
+
+    if(this.xPos === this.tx && this.yPos === this.ty) {
+        this.isMoving = false;
+        this.dirX = 0;
+        this.dirY = 0;
+        }
+    }
+}
+
+display() {
+    imageMode(CORNER);
+    image(this.sprite, this.xPos, this.yPos, this.size, this.size);
+}
+
+}
+let spotlightRadius = 150; // how big the radius of the spotlight is
+
+
+class Tile {
+    constructor(texture, x, y, tileSize, tileID,item=null) {
+        this.texture = texture;
+        this.x = x;
+        this.y = y;
+        this.tileSize = tileSize;
+        this.tileID = tileID;
+        this.visible = false;//Adding visibility
+        this.item=item;//to save  gun&battery
+    }
+    display() {
+        // check if the tile is within the visibility
+        if (dist(player.xPos, player.yPos, this.x + this.tileSize / 2, this.y + this.tileSize / 2) < spotlightRadius) {
+            this.visible = true;
+        } else {
+            this.visible = false;
+        }
+
+        
+        if (this.visible) {
+            noStroke();
+            image(this.texture, this.x, this.y, this.tileSize, this.tileSize);
+        }
+
+        // check if the item is within the visibility
+        if (this.item && this.visible) {
+            // only displaying the item if it's within the spotlight area
+            image(this.item, this.x, this.y, this.tileSize, this.tileSize);
+        }
+    }
+    debug() {
+        //TILE
+        stroke(245);
+        fill(55);
+        rect(this.x, this.y, this.tileSize, this.tileSize);
+
+        //LABEL
+        noStroke();
+        fill(245);
+        textAlign(LEFT, TOP);
+        text(this.tileID, this.x, this.y);
+    }
+}
+
+
+//code for the start menu
+//let MENU = 0
+//let img2;
+
+//function preload() {
+ // img2 = loadImage('IMG_1716.PNG');
+//}
+
+//function setup() {
+ // createCanvas(1000, 1000);
+//}
+
+//function draw() {
+//  print(mouseX, mouseY)
+ // background(0);
+ // fill(0, 255, 0, 50);
+ // rect(50, 50, 200, 75);
+  //fill(255, 0, 255, 50);
+  //rect(50, 200, 200, 75);
+ // textSize(50)
+ // fill(255);
+  //text('START', 70, 106);
+  
+  //textSize(26);
+ // text('INSTRUCTIONS', 52, 248);
+ // image(img2, 250, 0, 800, 800);
+
+  //if (MENU == 1) {
+    
+   // fill(0)
+    //textSize(20)
+   // text('Right Click to return to MENU', 525, 30)
+   // if (mouseButton == RIGHT) {
+   //   MENU = 0
+  //  }
+  //} // START GAME
+ // if (MENU == 2) {
+//    background(0)
+ //   textSize(20)
+//    text('Right Click to return to MENU', 525, 30)
+//    textSize(30)
+ //   text('-use WASD to move the character', 50, 150)
+//    text('-navigate the maze', 50, 200)
+ //   text('-pick up batteries to expand your view', 50, 240)
+ //   text('-defeat enemies', 50, 290)
+ //   if (mouseButton == RIGHT) {
+ //     MENU = 0
+ //   }
+//  } // INSTRUCTIONS
+
+//}
+
+//function mouseClicked() {
+
+//    Code for torch sfx if i ever get it working
+//    sfx.stop();
+//    userStartAudio();
+//    sfx.stop();
+//    if (torchClick = true) {
+//        sfx.play();
+//    }
+
+
+//  if (MENU == 0) {
+//    if (mouseX < 200 && mouseX > 50) {
+//      if (mouseY < 125 && mouseY > 50) {
+  //      MENU = 1
+//      }
+ //     if (mouseY < 275 && mouseY > 200) {
+//        MENU = 2
+ //     }
+      
+ //   }
+//  }
+
